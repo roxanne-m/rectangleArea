091f2a799f1e3fb6528fa10378e577e85a0f5eae
Roxanne <roxanne-m@users.noreply.github.com>
2021-08-14 00:27:38 -0700

    final working version

---
+++ rectangleArea.js
@@ -0,0 +1,54 @@
+// Find the area of a rectangle given x & y coordinates.
+
+function ArrayChallenge(strArr) {
+  // Check if passed in correct amount of coordinates
+  if (strArr.length < 4) {
+    throw new Error(
+      'Invalid amount of coordinates passed. Four coordinates are requried to determine area of a rectangle.'
+    );
+  }
+
+  let values = strArr.map((coord) => coord.match(/\d/g));
+
+  let finalValues = values.map((IntValues) =>
+    IntValues.map((val) => parseInt(val))
+  );
+
+  let width = [finalValues[0], finalValues[1]];
+  let height = [finalValues[2], finalValues[3]];
+
+  //   console.log(width, 'WIDTH');
+  //   console.log(height, 'HEIGHT');
+
+  // arrW = [1, 1, 1, 3]
+  let arrW = [];
+
+  // arrL = [3, 1, 3, 3]
+  let arrL = [];
+  let one = width.forEach((a) => a.forEach((b) => arrW.push(b)));
+  let two = height.forEach((a) => a.forEach((b) => arrL.push(b)));
+
+  //   console.log(arrW, 'ARRW');
+  //   console.log(arrL, 'ARRL');
+
+  let finalW = Math.sqrt(
+    Math.pow(arrW[0] - arrW[3], 2) + Math.pow(arrW[1] - arrW[2], 2)
+  );
+
+  let finalL = Math.sqrt(
+    Math.pow(arrL[0] - arrL[3], 2) + Math.pow(arrL[1] - arrL[2], 2)
+  );
+  //   console.log(finalW, 'FINAL W');
+  //   console.log(finalL, 'FINAL L');
+
+  let rectangleArea = finalW * finalL;
+
+  return rectangleArea;
+}
+
+let array1 = ['(1 1)', '(1 3)', '(3 1)', '(3 3)'];
+let array2 = ['(0 0)', '(1 0)', '(1 1)', '(0 1)'];
+
+// keep this function call here
+console.log(ArrayChallenge(array1));
+console.log(ArrayChallenge(array2));
